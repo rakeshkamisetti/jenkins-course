6ca47f286158d884d53e605f9ec50dc305ccf7c9
rkamisetti <rakeshkamisetti@gmail.com>
2018-05-19 11:21:17 +0530

    update git repository and email

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git@github.com:rakeshkamisetti/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('rakeshkamisetti@gmail.com')
         }
     }
     triggers {
