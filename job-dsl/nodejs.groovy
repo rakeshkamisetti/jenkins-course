0c4186fd82a4ed8f14f1b448ca7d54fa2aa199fe
rkamisetti <rakeshkamisetti@gmail.com>
2018-05-24 05:48:25 +0530

    change job name

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,4 +1,4 @@
-job('NodeJS example') {
+job('NodeJS-example-script') {
     scm {
         git('https://github.com/rakeshkamisetti/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
